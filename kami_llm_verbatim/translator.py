6040b991d6b538b87a6da11134edddd4cdf954ed
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-11-09 00:19:20 -0800

    add SubtitleTranslator class with placeholder and init storing episode instance
    [^]translator.py

---
+++ kami_llm_verbatim/translator.py
@@ -0,0 +1,17 @@
+"""
+implement ``SubtitleTranslator``
+"""
+
+from .episode import SubtitleEpisode
+
+
+class SubtitleTranslator:
+    """
+    TODO
+
+    :param episode:
+    :type episode: SubtitleTranslator
+    """
+
+    def __init__(self, episode):
+        self.episode = episode
