a000a17d88b117e7f41de05d236926b68e8cb6d7
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-11-09 00:47:23 -0800

    move Translate Logic, Worker, Constants: __init__.py to translator.py, clarify module focus
    [-]__init__.py: remove translation worker and related code, keep only imports and exports for subtitle episodes
    [|]translator.py: add parallel subtitle translation function with process pool executor; implement basic worker swapping case of lines; update translated lines asynchronously; mark TODO for class refactor; preserve SubtitleTranslator class unchanged

--- kami_llm_verbatim/translator.py
+++ kami_llm_verbatim/translator.py
@@ -2,6 +2,7 @@
 implement ``SubtitleTranslator``
 """
 
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from .episode import SubtitleEpisode
 
 
@@ -15,3 +16,41 @@ class SubtitleTranslator:
 
     def __init__(self, episode):
         self.episode = episode
+
+
+# TODO refactor as a class
+
+MAX_WORKERS = 8
+LINES_PER_RESPONSE = 2  # HACK
+
+
+def _translation_worker(episode, i):
+    end = (i + 1) * LINES_PER_RESPONSE
+    if end > len(episode):  # special case last set
+        end = len(episode)
+
+    translated_lines = []
+
+    for line in episode.lines[i * LINES_PER_RESPONSE : end]:
+        translated_lines.append(line.swapcase())  # HACK
+
+    return (i, translated_lines)
+
+
+def translate(src_file_path, dest_file_path):
+
+    episode_type = SrtSubtitleEpisode  # HACK
+
+    with episode_type(src_file_path, dest_file_path) as episode:
+        updates = []
+        with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
+            futures = [
+                executor.submit(_translation_worker, episode, i)
+                for i in range(len(episode))
+            ]
+
+            for future in as_completed(futures):
+                updates.append(future.result())  # BUG
+
+        for i, payload in updates:
+            episode.translated_lines[i * LINES_PER_RESPONSE] = payload
